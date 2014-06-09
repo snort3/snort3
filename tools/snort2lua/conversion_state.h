a99c5058f687e6039b1555a04df7a3a76b566c39
Josh <jrosenba@cisco.com>
2014-06-09 18:32:33 -0400

    instantiates each state before entering.  correctly parses portvar.

---
+++ tools/snort2lua/conversion_state.h
@@ -0,0 +1,60 @@
+/*
+** Copyright (C) 2014 Cisco and/or its affiliates. All rights reserved.
+ * Copyright (C) 2002-2013 Sourcefire, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 2 as
+ * published by the Free Software Foundation.  You may not use, modify or
+ * distribute this program under any other version of the GNU General
+ * Public License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// converter.h author Josh Rosenbaum <jorosenba@cisco.com>
+
+#ifndef CONVERSION_STATE_H
+#define CONVERSION_STATE_H
+
+#include <string>
+#include <fstream>
+
+class Converter;
+
+class ConversionState
+{
+
+public:
+    ConversionState(Converter *cv){ converter = cv; }
+    virtual ~ConversionState() {};
+    virtual bool convert(std::stringstream& data, 
+                            bool last_line, 
+                            std::ofstream& out)=0;
+
+
+protected:
+    Converter* converter;
+
+private:
+
+};
+
+
+typedef ConversionState* (*conv_new_f)(Converter*);
+
+struct ConvertMap
+{
+    std::string keyword;
+    conv_new_f ctor;
+};
+
+
+
+
+#endif
