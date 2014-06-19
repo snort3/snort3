70d71d643f7daa78e93c09e65112a7c16bf4158b
Russ Combs <rucombs@cisco.com>
2014-06-18 23:08:44 -0400

    removed unused contents from hi

--- src/service_inspectors/http_inspect/http_inspect.cc
+++ src/service_inspectors/http_inspect/http_inspect.cc
@@ -482,18 +482,6 @@ static void hs_reset()
 
 //-------------------------------------------------------------------------
 
-static const char* contents =
-    "http_client_body "
-    "http_cookie "
-    "http_raw_cookie "
-    "http_header "
-    "http_raw_header "
-    "http_method "
-    "http_uri "
-    "http_raw_uri  "
-    "http_stat_code "
-    "http_stat_msg ";
-
 static const InspectApi hs_api =
 {
     {
