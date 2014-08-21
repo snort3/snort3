846b8e1364dfefcc2eb875960f268be25f0c0c74
rcombs <rcombs@sq18.sfeng.sourcefire.com>
2014-08-21 18:41:56 -0400

    SO_PUBLICations

--- src/stream/stream_splitter.cc
+++ src/stream/stream_splitter.cc
@@ -29,10 +29,10 @@
 static THREAD_LOCAL uint8_t pdu_buf[65536];
 static THREAD_LOCAL StreamBuffer str_buf;
 
-uint32_t StreamSplitter::max()
+SO_PUBLIC uint32_t StreamSplitter::max()
 { return 16384; }  // FIXIT-H make default configurable
 
-const StreamBuffer* StreamSplitter::reassemble(
+SO_PUBLIC const StreamBuffer* StreamSplitter::reassemble(
     Flow*, unsigned, unsigned offset, const uint8_t* p,
     unsigned n, uint32_t flags, unsigned& copied)
 { 
