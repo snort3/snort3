8fa7b37aaa50ccc0a9fbeb05a9b28378799521ec
Josh <jrosenba@cisco.com>
2014-11-20 13:46:05 -0600

    more verdict count fixes

--- src/flow/flow_control.cc
+++ src/flow/flow_control.cc
@@ -380,7 +380,7 @@ unsigned FlowControl::process(Flow* flow, Packet* p)
     case Flow::BLOCK:
         // FIXIT-M should not repeatedly clear session
         stream.drop_packet(p);
-        Active_DropSession();
+        Active_DropPacket();
         break;
     }
 
