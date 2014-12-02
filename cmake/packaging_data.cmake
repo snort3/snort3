8603d1803bfa7ce90019ac245ee36531970f908e
Josh <jrosenba@cisco.com>
2014-12-02 11:39:30 -0600

    updating CPack. Now imitiates autotools 'make dist' and packages binaries rather than symlinks

--- cmake/packaging_data.cmake
+++ cmake/packaging_data.cmake
@@ -1,16 +1,35 @@
 
 # use this target instead of 'make package_source'
-add_custom_target(autotools
+add_custom_target( autotools
     COMMAND autoreconf -isvf #  FIXIT-L J  --  should check for autotools the CMake way
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 )
 
-add_custom_target(dist
+add_custom_target( copy_symlinks
+    COMMAND
+        ${CMAKE_COMMAND} -DSOURCE_DIRECTORY=${CMAKE_SOURCE_DIR}
+        -P ${CMAKE_CURRENT_LIST_FILE}/copy_symlinks.cmake
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+)
+
+add_custom_target( copy_m4_symlinks
+    COMMAND
+        ${CMAKE_COMMAND}
+        -DSOURCE_DIRECTORY=${CMAKE_SOURCE_DIR}/m4
+        -P ${CMAKE_CURRENT_LIST_FILE}/copy_symlinks.cmake
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/m4
+)
+
+add_custom_target( dist
     COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target copy_manuals_to_source
     COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target autotools
+    COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target copy_symlinks
+    COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target copy_m4_symlinks
     COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target package_source
     COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target delete_manuals_in_source
+    COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target autotools  # recreate autotool links.
 )
+
 
 set (CPACK_GENERATOR TGZ)
 set (CPACK_PACKAGE_NAME "snort")
@@ -22,8 +41,7 @@ set (CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/doc/images/snort.png")
 set (CPACK_PACKAGE_INSTALL_DIRECTORY "snort")
 set (CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/COPYING")
 set (CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/doc/start.txt")
-set (CPACK_SOURCE_IGNORE_FILES "${CMAKE_BINARY_DIR}/;tools/snort2lua/tests/;\\\\.git/;\\\\.gitignore;m4/;aclocal.m4;ylwrap;extra/")
+set (CPACK_SOURCE_IGNORE_FILES "${CMAKE_BINARY_DIR}/;tools/snort2lua/tests/;\\\\.git/;\\\\.gitignore;extra/;")
 set (CPACK_SOURCE_GENERATOR TGZ)
 
 include(CPack)
-
