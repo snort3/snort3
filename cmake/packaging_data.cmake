b0f1d853a02320e3f12db761739458766b055631
Josh <jrosenba@cisco.com>
2014-12-04 14:16:58 -0600

    initial removal of autotools from cmake

--- cmake/packaging_data.cmake
+++ cmake/packaging_data.cmake
@@ -1,25 +1,11 @@
 
-# use this target instead of 'make package_source'
-add_custom_target( autotools_binaries
-    COMMAND autoreconf -ivf #  FIXIT-L J  --  should check for autotools the CMake way
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMENT "reconfiguring all autotools files"
-)
-
-add_custom_target( autotools_symlinks
-    COMMAND autoreconf -isvf
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMENT "recreating autotools symlinks"
-)
-
 add_custom_target( dist
     COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target copy_manuals_to_source
-    COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target autotools_binaries
     COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target package_source
     COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target delete_manuals_in_source
-    COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target autotools_symlinks  # recreate autotool links.
 )
 
+set (AUTOTOOL_FILES "autom4te.cache;m4;configure.ac;Makefile.am")
 
 set (CPACK_GENERATOR TGZ)
 set (CPACK_PACKAGE_NAME "snort")
@@ -32,7 +18,7 @@ set (CPACK_PACKAGE_INSTALL_DIRECTORY "snort")
 set (CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/COPYING")
 set (CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/doc/start.txt")
 set (CPACK_SOURCE_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
-set (CPACK_SOURCE_IGNORE_FILES "tools/snort2lua/tests/;\\\\.git/;\\\\.gitignore;extra/;autom4te.cache;m4")
+set (CPACK_SOURCE_IGNORE_FILES "tools/snort2lua/tests/;\\\\.git/;\\\\.gitignore;extra/;${AUTOTOOL_FILES}")
 set (CPACK_SOURCE_GENERATOR TGZ)
 
 include(CPack)
