7ddab7a3656c1f2197d900a191ca2d34ff326327
Josh <jrosenba@cisco.com>
2014-12-11 12:03:52 -0600

    ading i4l_rawip and pflog roto codecs

--- src/codecs/root/cd_i4l_rawip.cc
+++ src/codecs/root/cd_i4l_rawip.cc
@@ -28,11 +28,16 @@
 #include "framework/codec.h"
 
 
-#ifdef DLT_I4L_RAWIP
 
-#define I4L_RAW_IP_NAME "i4l_rawip"
-#define I4L_RAW_IP_HELP_STR "support for I4L IP"
-#define I4L_RAW_IP_HELP ADD_DLT(I4L_RAW_IP_HELP_STR, DLT_I4L_RAWIP)
+#define I4L_RAWIP_NAME "i4l_rawip"
+#define I4L_RAWIP_HELP_STR "support for I4L IP"
+
+
+#ifdef DLT_I4L_RAWIP
+#define I4L_RAWIP_HELP ADD_DLT(I4L_RAWIP_HELP_STR, DLT_I4L_RAWIP)
+#else
+#define I4L_RAWIP_HELP I4L_RAWIP_HELP_STR
+#endif
 
 namespace
 {
@@ -40,7 +45,7 @@ namespace
 class I4LRawIpCodec : public Codec
 {
 public:
-    I4LRawIpCodec() : Codec(I4L_RAW_IP_NAME){};
+    I4LRawIpCodec() : Codec(I4L_RAWIP_NAME){};
     ~I4LRawIpCodec() {};
 
     void get_data_link_type(std::vector<int>&) override;
@@ -50,24 +55,16 @@ public:
 
 } // namespace
 
-
+#ifdef DLT_I4L_RAWIP
 void I4LRawIpCodec::get_data_link_type(std::vector<int>& v)
 { v.push_back(DLT_I4L_RAWIP); }
 
-/*
- * Function: DecodeI4LRawIPPkt(Packet *, char *, DAQ_PktHdr_t*, uint8_t*)
- *
- * Purpose: Decodes packets coming in raw on layer 2, like PPP.  Coded and
- *          in by Jed Pickle (thanks Jed!) and modified for a few little tweaks
- *          by me.
- *
- * Arguments: p => pointer to decoded packet struct
- *            user => Utility pointer, unused
- *            pkthdr => ptr to the packet header
- *            pkt => pointer to the real live packet data
- *
- * Returns: void function
- */
+#else
+void I4LRawIpCodec::get_data_link_type(std::vector<int>& v)
+{ }
+
+#endif
+
 
 bool I4LRawIpCodec::decode(const RawData& raw, CodecData& codec, DecodeData&)
 {
@@ -92,12 +89,12 @@ static void dtor(Codec *cd)
 { delete cd; }
 
 
-static const CodecApi i4l_raw_ip_api =
+static const CodecApi i4l_rawip_api =
 {
     {
         PT_CODEC,
-        I4L_RAW_IP_NAME,
-        I4L_RAW_IP_HELP,
+        I4L_RAWIP_NAME,
+        I4L_RAWIP_HELP,
         CDAPI_PLUGIN_V0,
         0,
         nullptr, // mod_ctor
@@ -115,11 +112,9 @@ static const CodecApi i4l_raw_ip_api =
 #ifdef BUILDING_SO
 SO_PUBLIC const BaseApi* snort_plugins[] =
 {
-    &i4l_raw_ip_api.base,
+    &i4l_rawip_api.base,
     nullptr
 };
 #else
-const BaseApi* cd_name = &i4l_raw_ip_api.base;
+const BaseApi* cd_i4l_rawip = &i4l_rawip_api.base;
 #endif
-
-#endif /* DLT_I4L_RAWIP */
