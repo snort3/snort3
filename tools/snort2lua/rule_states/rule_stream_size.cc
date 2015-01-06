f5bd5ad3a95b8bda7fae5131cc4fbb016722c719
Josh <jrosenba@cisco.com>
2015-01-06 15:22:49 -0600

    adding stream_size option.  New option is 'stream_size:direction,range'

---
+++ tools/snort2lua/rule_states/rule_stream_size.cc
@@ -0,0 +1,108 @@
+//--------------------------------------------------------------------------
+// Copyright (C) 2014-2015 Cisco and/or its affiliates. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License Version 2 as published
+// by the Free Software Foundation.  You may not use, modify or distribute
+// this program under any other version of the GNU General Public License.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//--------------------------------------------------------------------------
+// rule_stream_size.cc author Josh Rosenbaum <jrosenba@cisco.com>
+
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+
+#include "conversion_state.h"
+#include "helpers/converter.h"
+#include "rule_states/rule_api.h"
+#include "helpers/s2l_util.h"
+
+namespace rules
+{
+
+namespace {
+
+
+class StreamSize : public ConversionState
+{
+public:
+    StreamSize(Converter& c) : ConversionState(c) {};
+    virtual ~StreamSize() {};
+    virtual bool convert(std::istringstream& data);
+};
+
+} // namespace
+
+bool StreamSize::convert(std::istringstream& data_stream)
+{
+    std::string args;
+
+    args = util::get_rule_option_args(data_stream);
+    std::istringstream arg_stream(args);
+
+    std::string dir = "";
+    std::string op = "";
+    std::string size = "";
+
+    if (!util::get_string(arg_stream, dir, ",") ||
+        !util::get_string(arg_stream, op, ",") ||
+        !util::get_string(arg_stream, size, ","))
+    {
+        rule_api.bad_rule(data_stream, "stream_size requires 3 arguments.  '<direction>,<operator>,<size>'");
+        return set_next_rule_state(data_stream);
+    }
+
+
+
+    if (!(!op.compare("=")  || !op.compare("<")  || !op.compare(">") ||
+          !op.compare("!=") || !op.compare("<=") || !op.compare(">=")))
+    {
+        rule_api.bad_rule(data_stream, "'" + op + "' in an invalid stream_size operator.");
+    }
+
+    try
+    {
+        // checking that this is a valid unsigned integer.
+        char* end;
+        std::strtoul(size.c_str(), &end, 10);
+    }
+    catch (const std::invalid_argument& e)
+    {
+        rule_api.bad_rule(data_stream, "stream_size <size> '" + size + "' is too large.");
+    }
+    catch (const std::out_of_range& e)
+    {
+        rule_api.bad_rule(data_stream, "stream_size <size> '" + size + "' is too large.");
+    }
+
+
+    rule_api.add_option("stream_size", dir);
+    rule_api.add_suboption(op + size);
+    return set_next_rule_state(data_stream);
+}
+
+/**************************
+ *******  A P I ***********
+ **************************/
+
+static ConversionState* stream_size_ctor(Converter& c)
+{ return new StreamSize(c); }
+
+static const ConvertMap rule_stream_size =
+{
+    "stream_size",
+    stream_size_ctor,
+};
+
+const ConvertMap* stream_size_map = &rule_stream_size;
+
+} // namespace rules
