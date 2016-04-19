9b3709a656ad3d49bb3b58f0d467d30528d44372
Joel Cornett <joel.cornett@gmail.com>
2016-04-19 16:29:07 -0400

    normalized pkgconfig output

--- cmake/create_pkg_config.cmake
+++ cmake/create_pkg_config.cmake
@@ -1,32 +1,66 @@
 # creating pkg-config module.  these will all be substituted into snort.pc
 
-foreach(flag COMPILE_DEFINITIONS COMPILE_FLAGS COMPILE_OPTIONS)
-    set(CPPFLAGS "${CPPFLAGS} ${${flag}}")
-endforeach()
+unset ( CPPDEFS )
 
-get_target_property(snort_includes snort INCLUDE_DIRECTORIES)
-get_target_property(compile_definitions snort COMPILE_DEFINITIONS)
-get_directory_property( dir_defs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} COMPILE_DEFINITIONS)
+# some ugly hacks to tease out defines that snort headers depend on.
+# these need to match in order to build dynamic snort plugins using pkgconfig
+# FIXIT-H J we need a better system to manage these exported defines. In the meantime,
+# any definitions that are depended on by installed snort headers need to end up in CPPDEFS
 
-string (REPLACE ";" " -I" TMP_FLAGS  "-I${EXTERNAL_INCLUDES}")
-string (REPLACE ";" " -D" TMP_DEFS  "-D${dir_defs}")
-string (REPLACE ";" " " LIBS  "${EXTERNAL_LIBRARIES}")
+get_directory_property( TMP_DEFS DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} COMPILE_DEFINITIONS)
 
-set(CPPFLAGS "${CPPFLAGS} ${TMP_FLAGS} ${TMP_DEFS}")
+if ( WORDS_MUSTALIGN )
+    list ( APPEND TMP_DEFS "WORDS_MUSTALIGN" )
+endif ()
+
+if ( BUILD_PIGLET )
+    list ( APPEND TMP_DEFS "PIGLET" )
+endif ()
+
+if ( BUILD_SHELL )
+    list ( APPEND TMP_DEFS "BUILD_SHELL" )
+endif ()
+
+if ( NOT ENABLE_COREFILES )
+    list ( APPEND TMP_DEFS "NOCOREFILE" )
+endif ()
+
+if ( BUILD_UNIT_TESTS )
+    list ( APPEND TMP_DEFS "UNIT_TEST" )
+endif ()
+
+if ( HAVE_LZMA )
+    list ( APPEND TMP_DEFS "HAVE_LZMA" )
+endif ()
+
+# attach -D to each define
+foreach ( def ${TMP_DEFS} )
+    if ( NOT ${def} MATCHES "HAVE_CONFIG_H" )
+        if ( NOT ${def} MATCHES "restrict" )
+            set ( CPPDEFS "${CPPDEFS} -D${def}" )
+        endif ()
+    endif ()
+endforeach  ( def )
+
+# set pkgconfig vars
+
 set(prefix "${CMAKE_INSTALL_PREFIX}")
-set(execprefix "\${prefix}")
-set(bindir "\${prefix}/bin")
+set(exec_prefix "\${prefix}")
+set(bindir "\${exec_prefix}/bin")
+set(libdir "\${exec_prefix}/snort")
 set(includedir "\${prefix}/include")
-set(libdir "\${prefix}/snort")
+set(datarootdir "\${prefix}/share")
+set(datadir "\${datarootdir}")
+set(mandir "\${datarootdir}/info")
+set(infodir "\${datarootdir}/info")
 
-
-
-# setting and install the pkg-config information
+# create & install pkgconfig file
 
 configure_file(
     "${CMAKE_SOURCE_DIR}/snort.pc.in"
     "${CMAKE_BINARY_DIR}/snort.pc"
-    @ONLY)
+    @ONLY
+)
 
 install (FILES ${CMAKE_BINARY_DIR}/snort.pc
     DESTINATION "lib/pkgconfig/"
