7737b784359729f4084fe1a1184399eb68570a40
huica <huica@cisco.com>
2016-05-09 14:15:51 -0400

    move file module to file api

--- src/file_api/file_config.cc
+++ src/file_api/file_config.cc
@@ -37,6 +37,7 @@
 #include "file_lib.h"
 #include "file_identifier.h"
 #include "parser/parse_utils.h"
+#include "main/snort_config.h"
 
 bool FileConfig::process_file_magic(FileMagicData &magic)
 {
@@ -88,4 +89,179 @@ std::string FileConfig::file_type_name( uint32_t id)
         return info->type;
 
     return "";
+}
+
+
+bool FileIdModule::set(const char*, Value& v, SnortConfig* sc)
+{
+    FileConfig& fc = sc->file_config;
+
+    FilePolicy& fp = fc.get_file_policy();
+
+    if ( v.is("type_depth") )
+        fc.file_type_depth = v.get_long();
+
+    else if ( v.is("signature_depth") )
+        fc.file_signature_depth = v.get_long();
+
+    else if ( v.is("block_timeout") )
+        fc.file_block_timeout = v.get_long();
+
+    else if ( v.is("lookup_timeout") )
+        fc.file_lookup_timeout = v.get_long();
+
+    else if ( v.is("block_timeout_lookup") )
+        fc.block_timeout_lookup = v.get_bool();
+
+    else if ( v.is("capture_memcap") )
+        fc.capture_memcap = v.get_long();
+
+    else if ( v.is("capture_max_size") )
+        fc.capture_max_size = v.get_long();
+
+    else if ( v.is("capture_min_size") )
+        fc.capture_min_size = v.get_long();
+
+    else if ( v.is("capture_block_size") )
+        fc.capture_block_size = v.get_long();
+
+    else if ( v.is("enable_type") )
+    {
+        if ( v.get_bool() )
+        {
+            fp.set_file_type(true);
+        }
+    }
+    else if ( v.is("enable_signature") )
+    {
+        if ( v.get_bool() )
+        {
+            fp.set_file_signature(true);
+        }
+    }
+    else if ( v.is("enable_capture") )
+    {
+        if ( v.get_bool() )
+        {
+            fp.set_file_capture(true);
+        }
+    }
+    else if ( v.is("show_data_depth") )
+        FileConfig::show_data_depth = v.get_long();
+
+    else if ( v.is("trace_type") )
+        FileConfig::trace_type = v.get_bool();
+
+    else if ( v.is("trace_signature") )
+        FileConfig::trace_signature = v.get_bool();
+
+    else if ( v.is("trace_stream") )
+        FileConfig::trace_stream = v.get_bool();
+
+    else if ( v.is("file_rules") )
+        return true;
+
+    else if ( v.is("rev") )
+        rule.rev = v.get_long();
+
+    else if ( v.is("msg") )
+        rule.message = v.get_string();
+
+    else if ( v.is("type") )
+        rule.type = v.get_string();
+
+    else if ( v.is("id") )
+        rule.id = v.get_long();
+
+    else if ( v.is("category") )
+        rule.category = v.get_string();
+
+    else if ( v.is("version") )
+        rule.version = v.get_string();
+
+    else if ( v.is("magic") )
+        return true;
+
+    else if ( v.is("content") )
+        magic.content_str = v.get_string();
+
+    else if ( v.is("offset") )
+        magic.offset = v.get_long();
+
+    else if ( v.is("file_policy") )
+        return true;
+
+    else if ( v.is("when") )
+        return true;
+
+    else if ( v.is("file_type_id") )
+        file_rule.when.type_id = v.get_long();
+
+    else if ( v.is("sha256") )
+        file_rule.when.sha256 = v.get_string();
+
+    else if ( v.is("use") )
+        return true;
+
+    else if ( v.is("verdict") )
+        file_rule.use.verdict = (FileVerdict)v.get_long();
+
+    else if ( v.is("enable_file_type") )
+        file_rule.use.type_enabled = v.get_bool();
+
+    else if ( v.is("enable_file_signature") )
+        file_rule.use.signature_enabled = v.get_bool();
+
+    else if ( v.is("enable_file_capture") )
+        file_rule.use.capture_enabled = v.get_bool();
+
+    else
+        return false;
+
+    return true;
+}
+
+bool FileIdModule::begin(const char* fqn, int idx, SnortConfig*)
+{
+    if (!idx)
+        return true;
+
+    if ( !strcmp(fqn, "file_id.file_rules") )
+    {
+        rule.clear();
+    }
+    else if ( !strcmp(fqn, "file_id.file_rules.magic") )
+    {
+        magic.clear();
+    }
+    else if ( !strcmp(fqn, "file_id.file_policy") )
+    {
+        file_rule.clear();
+    }
+
+    return true;
+}
+
+bool FileIdModule::end(const char* fqn, int idx, SnortConfig* sc)
+{
+    FileConfig& fc = sc->file_config;
+
+    if (!idx)
+        return true;
+
+    if ( !strcmp(fqn, "file_id.file_rules") )
+    {
+        fc.process_file_rule(rule);
+    }
+    else if ( !strcmp(fqn, "file_id.file_rules.magic") )
+    {
+        fc.process_file_magic(magic);
+        rule.file_magics.push_back(magic);
+    }
+    else if ( !strcmp(fqn, "file_id.file_policy") )
+    {
+        fc.process_file_policy_rule(file_rule);
+    }
+
+    return true;
 }
