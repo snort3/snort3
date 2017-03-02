0be99468879bc211200c64564fb06e0cd8fd29aa
Justin Viiret <justin.viiret@intel.com>
2017-03-03 10:03:58 +1100

    cmake: require Hyperscan >= 4.4.0
    This version is required for the hs_valid_platform() function.

--- cmake/FindHS.cmake
+++ cmake/FindHS.cmake
@@ -10,6 +10,6 @@ find_library(HS_LIBRARIES NAMES hs
     HINTS ${HS_LIBRARIES_DIR} ${PC_HYPERSCAN_LIBDIR} ${PC_HYPERSCAN_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(HS DEFAULT_MSG HS_LIBRARIES HS_INCLUDE_DIRS)
+find_package_handle_standard_args(HS REQUIRED_VARS HS_LIBRARIES HS_INCLUDE_DIRS VERSION_VAR PC_HYPERSCAN_VERSION)
 
 mark_as_advanced(HS_INCLUDE_DIRS HS_LIBRARIES)
