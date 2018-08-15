87529ce9807ae4c84217b40fb4973e5487d6016d
Russ Combs (rucombs) <rucombs@cisco.com>
2018-08-14 20:48:03 -0400

    Merge pull request #1331 in SNORT/snort3 from build_246 to master
    Squashed commit of the following:
    
    commit 916ca28fdb132d667324f795b8f9487981830c18
    Author: Russ Combs <rucombs@cisco.com>
    Date:   Mon Aug 13 21:41:30 2018 -0400
    
        build: 246
        snort.lua: fix tweaks path; thanks to brastult@cisco.com for reporting the issue
        snort.lua: fix community rules filename; thanks to mike@flyn.org for reporting the issue

--- src/main/build.h
+++ src/main/build.h
@@ -12,7 +12,7 @@
 //                                               //
 //-----------------------------------------------//
 
-#define BUILD_NUMBER 245
+#define BUILD_NUMBER 246
 
 #ifndef EXTRABUILD
 #define BUILD STRINGIFY_MX(BUILD_NUMBER)
