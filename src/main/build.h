bdef59525b892814caebb3c77bdb3813ad5702f2
Russ Combs (rucombs) <rucombs@cisco.com>
2019-05-04 12:23:41 -0400

    Merge pull request #1595 in SNORT/snort3 from ~RUCOMBS/snort3:build_255 to master
    Squashed commit of the following:
    
    commit 5dc88fa07e26e6332fcc681e8d6e7ff0321e712e
    Author: Russ Combs <rucombs@cisco.com>
    Date:   Fri May 3 18:34:44 2019 -0400
    
        build: generate and tag build 255

--- src/main/build.h
+++ src/main/build.h
@@ -12,7 +12,7 @@
 //                                               //
 //-----------------------------------------------//
 
-#define BUILD_NUMBER 254
+#define BUILD_NUMBER 255
 
 #ifndef EXTRABUILD
 #define BUILD STRINGIFY_MX(BUILD_NUMBER)
