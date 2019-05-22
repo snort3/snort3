b2976b29108b1248e7ad672443d217735fd25f55
Mike Stepanek (mstepane) <mstepane@cisco.com>
2019-05-22 16:41:32 -0400

    Merge pull request #1615 in SNORT/snort3 from ~NIHDESAI/snort3:build_256 to master
    Squashed commit of the following:
    
    commit 68d6c61b35320abc1301d55a0bac95d57e25ec3c
    Author: Nihal Desai <nihdesai@cisco.com>
    Date:   Wed May 22 13:42:31 2019 -0400
    
        build: generate build 256

--- src/main/build.h
+++ src/main/build.h
@@ -12,7 +12,7 @@
 //                                               //
 //-----------------------------------------------//
 
-#define BUILD_NUMBER 255
+#define BUILD_NUMBER 256
 
 #ifndef EXTRABUILD
 #define BUILD STRINGIFY_MX(BUILD_NUMBER)
