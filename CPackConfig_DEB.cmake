28f6e8db31051647a1ed9a9b8941d7dfb32c671b
ubuntu <ubuntu@k8s-local-dev>
2019-07-10 12:14:50 +0000

    add deb conf

---
+++ CPackConfig_DEB.cmake
@@ -0,0 +1,96 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+SET(CPACK_BINARY_7Z "")
+SET(CPACK_BINARY_BUNDLE "")
+SET(CPACK_BINARY_CYGWIN "")
+SET(CPACK_BINARY_DEB "")
+SET(CPACK_BINARY_DRAGNDROP "")
+SET(CPACK_BINARY_FREEBSD "")
+SET(CPACK_BINARY_IFW "")
+SET(CPACK_BINARY_NSIS "")
+SET(CPACK_BINARY_OSXX11 "")
+SET(CPACK_BINARY_PACKAGEMAKER "")
+SET(CPACK_BINARY_PRODUCTBUILD "")
+SET(CPACK_BINARY_RPM "")
+SET(CPACK_BINARY_STGZ "")
+SET(CPACK_BINARY_TBZ2 "")
+SET(CPACK_BINARY_TGZ "")
+SET(CPACK_BINARY_TXZ "")
+SET(CPACK_BINARY_TZ "")
+SET(CPACK_BINARY_WIX "")
+SET(CPACK_BINARY_ZIP "")
+SET(CPACK_BUILD_SOURCE_DIRS "/home/ubuntu/snort3;/home/ubuntu/snort3/build")
+SET(CPACK_CMAKE_GENERATOR "Unix Makefiles")
+SET(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+SET(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+SET(CPACK_GENERATOR "DEB")
+SET(CPACK_INSTALL_CMAKE_PROJECTS "/home/ubuntu/snort3/build;snort;ALL;/")
+SET(CPACK_INSTALL_PREFIX "/usr")
+SET(CPACK_MODULE_PATH "/home/ubuntu/snort3/cmake")
+SET(CPACK_NSIS_DISPLAY_NAME "snort")
+SET(CPACK_NSIS_INSTALLER_ICON_CODE "")
+SET(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+SET(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+SET(CPACK_NSIS_PACKAGE_NAME "snort")
+SET(CPACK_OUTPUT_CONFIG_FILE "/home/ubuntu/snort3/build/CPackConfig.cmake")
+SET(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+SET(CPACK_PACKAGE_DESCRIPTION_FILE "/usr/share/cmake-3.10/Templates/CPack.GenericDescription.txt")
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "snort built using CMake")
+SET(CPACK_PACKAGE_ICON "/home/ubuntu/snort3/doc/images/snort.png")
+SET(CPACK_PACKAGE_INSTALL_DIRECTORY "snort")
+SET(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "snort")
+SET(CPACK_PACKAGE_NAME "dojo-snort")
+SET(CPACK_PACKAGE_RELOCATABLE "true")
+SET(CPACK_PACKAGE_VENDOR "Cisco")
+#SET(CPACK_PACKAGE_VERSION_MAJOR "3")
+#SET(CPACK_PACKAGE_VERSION_MINOR "0")
+#SET(CPACK_PACKAGE_VERSION_PATCH "0")
+SET(CPACK_RESOURCE_FILE_LICENSE "/home/ubuntu/snort3/LICENSE")
+SET(CPACK_RESOURCE_FILE_README "/home/ubuntu/snort3/README.md")
+SET(CPACK_RESOURCE_FILE_WELCOME "/usr/share/cmake-3.10/Templates/CPack.GenericWelcome.txt")
+SET(CPACK_SET_DESTDIR "OFF")
+SET(CPACK_SOURCE_7Z "")
+SET(CPACK_SOURCE_CYGWIN "")
+SET(CPACK_SOURCE_GENERATOR "DEB")
+SET(CPACK_SOURCE_IGNORE_FILES "tools/snort2lua/tests/;\\.git/;\\.gitignore;extra/;/home/ubuntu/snort3/build/*")
+SET(CPACK_SOURCE_OUTPUT_CONFIG_FILE "/home/ubuntu/snort3/build/CPackSourceConfig.cmake")
+SET(CPACK_SOURCE_PACKAGE_FILE_NAME "snort-3.0.0")
+SET(CPACK_SOURCE_RPM "")
+SET(CPACK_SOURCE_TBZ2 "")
+SET(CPACK_SOURCE_TGZ "")
+SET(CPACK_SOURCE_TXZ "")
+SET(CPACK_SOURCE_TZ "")
+SET(CPACK_SOURCE_ZIP "")
+SET(CPACK_SYSTEM_NAME "Linux")
+SET(CPACK_TOPLEVEL_TAG "Linux")
+SET(CPACK_WIX_SIZEOF_VOID_P "8")
+
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS "daq (>= 2.2.2)")
+SET(CPACK_PACKAGE_CONTACT "alon.dotan@bullguard.com")
+
+execute_process(COMMAND bash "-c" "git describe --exact-match --tags $(git log -n1 --pretty='%h') 2> /dev/null || git log -n1 --pretty='%h'" 
+		OUTPUT_VARIABLE GIT_VER
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+SET(CPACK_PACKAGE_VERSION "3-${GIT_VER}")
+SET(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}.${CPACK_PACKAGE_VERSION})
+#SET(CPACK_PACKAGE_FILE_NAME "dojo-snort-3.0.0")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "/home/ubuntu/snort3/build/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
