92e69315b247a50a5a435213f9f5ddc3c9f55e75
Michael Altizer (mialtize) <mialtize@cisco.com>
2020-07-06 14:45:41 +0000

    Merge pull request #2312 in SNORT/snort3 from ~MIALTIZE/snort3:3_0_2_build_1 to master
    Squashed commit of the following:
    
    commit b27307246e7cc6bdca8efef0f8112116c836bc3a
    Author: Michael Altizer <mialtize@cisco.com>
    Date:   Mon Jul 6 10:01:15 2020 -0400
    
        build: generate and tag 3.0.2 build 1

--- src/main/build.h
+++ src/main/build.h
@@ -12,7 +12,7 @@
 //                                               //
 //-----------------------------------------------//
 
-#define BUILD_NUMBER 0
+#define BUILD_NUMBER 1
 
 #ifndef EXTRABUILD
 #define BUILD STRINGIFY_MX(BUILD_NUMBER)
