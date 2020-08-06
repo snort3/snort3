fbb33198cb68874abda48e9892723f94c1c0baa3
Mike Stepanek (mstepane) <mstepane@cisco.com>
2020-08-06 12:15:50 +0000

    Merge pull request #2367 in SNORT/snort3 from ~MSTEPANE/snort3:3_0_2_build_4 to master
    Squashed commit of the following:
    
    commit 70e0c1d9a7e51c6f5edbd3b734bb9b68e36e8523
    Author: Mike Stepanek <mstepane@cisco.com>
    Date:   Fri Jul 31 08:12:24 2020 -0400
    
        build: generate and tag 3.0.2 build 4

--- src/main/build.h
+++ src/main/build.h
@@ -12,7 +12,7 @@
 //                                               //
 //-----------------------------------------------//
 
-#define BUILD_NUMBER 3
+#define BUILD_NUMBER 4
 
 #ifndef EXTRABUILD
 #define BUILD STRINGIFY_MX(BUILD_NUMBER)
