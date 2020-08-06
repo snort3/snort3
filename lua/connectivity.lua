6f3ae8ddf9da7daf382575c8aa0e0c5eee306c6a
Russ Combs (rucombs) <rucombs@cisco.com>
2020-08-06 14:48:08 +0000

    Merge pull request #2379 in SNORT/snort3 from ~RUCOMBS/snort3:tcp80 to master
    Squashed commit of the following:
    
    commit eb76760da445e01a1037f4cb6d45aaec3ba6568d
    Author: russ <rucombs@cisco.com>
    Date:   Wed Aug 5 09:03:12 2020 -0400
    
        lua: add tcp port 80 binding to the connectivity and balanced tweaks

--- lua/connectivity.lua
+++ lua/connectivity.lua
@@ -15,3 +15,7 @@ port_scan = nil
 
 stream_ip.min_frag_length = 16
 
+table.insert(
+    binder, 1, -- add http port binding to appease the perf gods
+    { when = { proto = 'tcp', ports = '80', role='server' }, use = { type = 'http_inspect' } })
+
