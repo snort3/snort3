beead962b9da0513d717ca68245473d63e16f087
Priyanka Bangalore Gurudev (prbg) <prbg@cisco.com>
2023-07-14 19:13:03 +0000

    Pull request #3914: ftp: remove file_data dependency on file_id
    Merge in SNORT/snort3 from ~PRBG/snort3:file_data_for_ftp_traffic to master
    
    Squashed commit of the following:
    
    commit f10bfb3e8f36f11d6170e85710d97a90b115fe5d
    Author: Priyanka Gurudev <prbg@cisco.com>
    Date:   Wed Jul 12 13:50:40 2023 -0400
    
        ftp: remove file_data dependency on file_id

--- src/service_inspectors/ftp_telnet/ftp_data.cc
+++ src/service_inspectors/ftp_telnet/ftp_data.cc
@@ -311,9 +311,6 @@ void FtpData::eval(Packet* p)
     // precondition - what we registered for
     assert(p->has_tcp_data());
 
-    if (FileService::get_max_file_depth() < 0)
-        return;
-
     SnortFTPData(p);
     ++fdstats.total_packets;
 }
