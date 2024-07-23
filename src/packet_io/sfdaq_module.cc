41ab0965cec83f8830296020e27c3c62e347686d
ARUNKUMAR KAYAMBU -X (akayambu - XORIANT CORPORATION at Cisco) <akayambu@cisco.com>
2024-07-23 18:49:12 +0000

    Pull request #4391: daq: added outstanding packets counter
    Merge in SNORT/snort3 from ~AKAYAMBU/snort3:outstanding_pkts to master
    
    Squashed commit of the following:
    
    commit 8cbc87c985ef43311fcecfe6574f3655294362e6
    Author: Arunkumar Kayambu <akayambu@cisco.com>
    Date:   Wed Jul 17 19:44:23 2024 -0400
    
        daq: added outstanding packets counter

--- src/packet_io/sfdaq_module.cc
+++ src/packet_io/sfdaq_module.cc
@@ -231,6 +231,7 @@ static DAQ_Stats_t operator-(const DAQ_Stats_t& left, const DAQ_Stats_t& right)
     ret.packets_received = left.packets_received - right.packets_received;
     ret.packets_filtered = left.packets_filtered - right.packets_filtered;
     ret.packets_injected = left.packets_injected - right.packets_injected;
+    ret.packets_outstanding = left.packets_outstanding - right.packets_outstanding;
 
     for ( unsigned i = 0; i < MAX_DAQ_VERDICT; i++ )
         ret.verdicts[i] = left.verdicts[i] - right.verdicts[i];
