18a629b30b744c4faa3db7dd74ac65a97e8399c9
Bl4omArchie <grlch.alexandre@proton.me>
2025-01-22 13:39:14 +0100

    update copyright

--- src/hash/hashes.h
+++ src/hash/hashes.h
@@ -15,7 +15,7 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //--------------------------------------------------------------------------
-// hashes.h author Alexandre Guerlach <grlch.alexandre@proton.me>
+// hashes.h author Russ Combs <rucombs@cisco.com>
 
 #ifndef HASHES_H
 #define HASHES_H
