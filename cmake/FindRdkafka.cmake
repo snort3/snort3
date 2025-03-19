8a3fbc93f7b8423485dfdf000d6a2b27bb27c754
Miguel Alvarez <malvarez@redborder.com>
2025-03-19 03:45:04 +0000

    delete comments

--- cmake/FindRdkafka.cmake
+++ cmake/FindRdkafka.cmake
@@ -1,13 +1,6 @@
 find_package(PkgConfig)
 pkg_check_modules(PC_RDKAFKA rdkafka)
 
-if(NOT DEFINED RDKAFKA_INCLUDE_DIR_HINT)
-  set(RDKAFKA_INCLUDE_DIR_HINT "" CACHE STRING "Hint for librdkafka include directory")
-endif()
-if(NOT DEFINED RDKAFKA_LIBRARIES_DIR_HINT)
-  set(RDKAFKA_LIBRARIES_DIR_HINT "" CACHE STRING "Hint for librdkafka library directory")
-endif()
-
 if (PC_RDKAFKA_FOUND)
   set(RDKAFKA_LIBRARIES ${PC_RDKAFKA_LIBRARIES})
   set(RDKAFKA_INCLUDE_DIRS ${PC_RDKAFKA_INCLUDE_DIRS})
@@ -25,12 +18,10 @@ find_library(RDKAFKA_LIBRARY
 
 if (RDKAFKA_INCLUDE_DIR AND RDKAFKA_LIBRARY)
   set(HAVE_RDKAFKA TRUE)
-  message(STATUS "librdkafka found: ${RDKAFKA_LIBRARY}")
 endif()
 
 if (NOT ENABLE_ALERT_KAFKA)
-    message(STATUS "Kafka alerts are disabled. Disabling librdkafka support.")
-    set(HAVE_RDKAFKA FALSE)
+  set(HAVE_RDKAFKA FALSE)
 endif()
 
 mark_as_advanced(
